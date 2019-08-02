c7b67ff68dfa610494e32819b9151ff548954022
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:54:18 +0800

    添加 WFun
    添加项目

---
+++ WFun/init.lua
@@ -0,0 +1,13 @@
+appver="1.0"
+appcode="1"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appname="万趣城"
+template="tool"
+packagename="com.android.WFun.com"
