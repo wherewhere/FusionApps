27be73ea5e35e451b28459ffa04b46eec5f151b4
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:59:55 +0800

    添加七天网络
    添加项目

---
+++ 七天网络/init.lua
@@ -0,0 +1,14 @@
+template="tool"
+appsdk="6"
+packagename="com.android.qitianwangluo"
+appname="七天网络"
+appcode="5"
+appver="0.2.2"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
