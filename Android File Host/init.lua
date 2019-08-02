62c736d8860e1498942a3475b16a206888661064
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:39:07 +0800

    添加 Android File Host
    添加项目

---
+++ Android File Host/init.lua
@@ -0,0 +1,13 @@
+appname="Android File Host"
+packagename="com.android.AndroidFileHost"
+appver="0.1.0"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appcode="1"
+template="tool"
