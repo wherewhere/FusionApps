7e7c529c46180ff9a6ae3cecd70c4f402a6069f4
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:55:34 +0800

    添加 Windows 93
    添加项目

---
+++ Windows 93/init.lua
@@ -0,0 +1,14 @@
+appname="Windows 93"
+appsdk="3"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+packagename="com.android.Windows93"
+template="blank"
+appcode="4"
+appver="0.2.1"
