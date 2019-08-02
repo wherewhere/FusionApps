d786c67042cd6021d86d7a870f633b35648d9dfa
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:45:26 +0800

    添加 QQ空间
    添加项目

---
+++ QQ空间/init.lua
@@ -0,0 +1,14 @@
+appcode="3"
+appsdk="1"
+packagename="com.android.QQzone"
+template="tool"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appname="QQ空间 Go"
+appver="0.2.0"
