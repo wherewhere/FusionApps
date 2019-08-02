f7b37658264bdd8c82736dbe5da05a223fe10857
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:58:05 +0800

    添加老D博客
    添加项目

---
+++ 老D博客/init.lua
@@ -0,0 +1,14 @@
+appname="老D博客"
+packagename="com.android.laoDboke"
+appver="0.1.0"
+appsdk="1"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+template="tool"
+appcode="1"
