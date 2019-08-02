5f4fcc50f934abae47326544bcdb13a342ba384e
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:42:13 +0800

    添加 APKMirror
    添加项目

---
+++ APKMirror/init.lua
@@ -0,0 +1,13 @@
+appname="APKMirror"
+appver="0.2.1"
+template="tool"
+packagename="com.android.APKMirror"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appcode="3"
