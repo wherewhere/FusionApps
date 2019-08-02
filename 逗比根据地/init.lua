dbd0dfa7b5a487e99357ec913f39e89fb41ae3aa
wherewhere <qq2518766683@outlook.com>
2019-08-02 20:56:26 +0800

    添加逗比根据地
    添加项目

---
+++ 逗比根据地/init.lua
@@ -0,0 +1,14 @@
+packagename="com.android.doub.io"
+appcode="2"
+appname="逗比根据地"
+template="tool"
+appver="0.2.0"
+appsdk="1"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
