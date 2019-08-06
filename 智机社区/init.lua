390dfced18ed8c47979edc2fbad8fda38d193ca6
wherewhere <qq2518766683@outlook.com>
2019-08-06 10:24:47 +0800

    添加 智机社区
    添加项目

---
+++ 智机社区/init.lua
@@ -0,0 +1,14 @@
+appsdk="1"
+template="tool"
+appname="智机社区"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+packagename="com.android.wfun"
+appver="0.1.0"
+appcode="1"
